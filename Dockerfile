2e75aaf51bf89a12d388ad5db8b47023c7c65596
Nemanja Badnjarevic <nemanja.badnjarevic@gmail.com>
2022-03-09 14:34:57 +0100

    Building docker file with mvn package, mvn deploy still broken

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM openjdk:11
-COPY target/vacation-service-0.0.1-SNAPSHOT.jar vacation-service-0.0.1-SNAPSHOT.jar
-ENTRYPOINT ["java","-jar","/vacation-service-0.0.1-SNAPSHOT.jar"]
+VOLUME /tmp
+ARG JAR_FILE
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/test/java/com/nbadnjarevic/vacationservice/config/TestRedisConfiguration.java
+++ src/test/java/com/nbadnjarevic/vacationservice/config/TestRedisConfiguration.java
@@ -2,28 +2,26 @@ package com.nbadnjarevic.vacationservice.config;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import redis.embedded.RedisServer;
 
-@TestConfiguration
+//@TestConfiguration
 public class TestRedisConfiguration {
 
   private RedisServer redisServer;
 
-  public TestRedisConfiguration(RedisProperties redisProperties) {
-    this.redisServer = RedisServer.builder().
-        port(redisProperties.getRedisPort())
-        .setting("maxmemory 128M")
-        .build();
+  public TestRedisConfiguration(@Value("${spring.redis.port}") final int redisPort) {
+    this.redisServer = new RedisServer(redisPort);
   }
 
   @PostConstruct
   public void postConstruct() {
-    redisServer.start();
+    this.redisServer.start();
   }
 
   @PreDestroy
   public void preDestroy() {
-    redisServer.stop();
+    this.redisServer.stop();
   }
 }
